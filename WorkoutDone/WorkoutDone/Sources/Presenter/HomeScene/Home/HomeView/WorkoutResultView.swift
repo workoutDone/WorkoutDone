0a6ea1827e3d5f3cac70726e97384ae3573c6f0e
ryuchanghwi <ckdgnl10@naver.com>
2023-03-03 15:07:35 +0900

    #1 - [Style] 홈뷰 운동결과 UIView 작업

--- WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/HomeView/WorkoutResultView.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/HomeView/WorkoutResultView.swift
@@ -20,4 +20,34 @@ class WorkoutResultView : BaseUIView {
         $0.backgroundColor = .colorF8F6FF
         $0.layer.cornerRadius = 12
     }
+    private let workoutTimeLabel = UILabel().then {
+        $0.textColor = .color7442FF
+        $0.text = "운동 시간"
+        $0.font = .pretendard(.semiBold, size: 12)
+    }
+    private let workoutTypeLabel = UILabel().then {
+        $0.textColor = .color7442FF
+        $0.text = "운동 종목"
+        $0.font = .pretendard(.semiBold, size: 12)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        addSubview(workoutResultLabel)
+        addSubview(workoutResultBaseView)
+    }
+    override func setupConstraints() {
+        super.setupConstraints()
+        workoutResultLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(30)
+            $0.height.equalTo(26)
+        }
+        workoutResultBaseView.snp.makeConstraints {
+            $0.top.equalTo(workoutResultLabel.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().offset(18)
+            $0.height.equalTo(82)
+        }
+    }
 }
