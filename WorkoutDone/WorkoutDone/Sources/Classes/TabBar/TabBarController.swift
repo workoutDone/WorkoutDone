6669163f91ad81ab80c39241031b2f4b4048800e
ryuchanghwi <ckdgnl10@naver.com>
2023-03-05 22:45:58 +0900

    #12 - UITabBarController 구현

---
+++ WorkoutDone/WorkoutDone/Sources/Classes/TabBar/TabBarController.swift
@@ -0,0 +1,26 @@
+//
+//  TabBarController.swift
+//  WorkoutDone
+//
+//  Created by 류창휘 on 2023/03/05.
+//
+
+import UIKit
+
+class TabBarController : UITabBarController {
+    let homeTab = HomeViewController()
+    let routineTab = RoutineViewController()
+    let myRecordTab = MyRecordViewController()
+    let homeTabBarItem = UITabBarItem(title: "오늘의 운동", image: UIImage(named: ""), tag: 0)
+    let routineTabBarItem = UITabBarItem(title: "운동 루틴", image: UIImage(named: ""), tag: 1)
+    let myRecordTabBarItem = UITabBarItem(title: "나의 기록", image: UIImage(named: ""), tag: 2)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [homeTab, routineTab, myRecordTab]
+        tabBar.isTranslucent = false
+        tabBar.barTintColor = .purple
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.blue
+    }
+}
---
+++ WorkoutDone/WorkoutDone/Sources/Presenter/MyRecordScene/MyRecord/MyRecordViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MyRecordViewController.swift
+//  WorkoutDone
+//
+//  Created by 류창휘 on 2023/03/05.
+//
+
+import UIKit
+
+class MyRecordViewController : BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
