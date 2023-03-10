cbd1baa519be4feaba530f8d2f832426b86e8271
hyemib <hyem0127@gmail.com>
2023-03-10 15:10:00 +0900

    #11 - [Feat] Calendar 구현

--- WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/HomeView/CalendarView.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/HomeView/CalendarView.swift
@@ -9,60 +9,114 @@ import UIKit
 import SnapKit
 import Then
 
-class CalendarView: BaseUIView {
+class CalendarView : BaseUIView {
     // MARK: - PROPERTIES
-    var currentMonth: Int = 3
+    var cal = Calendar.current
+    let dateFormatter = DateFormatter()
+    var components = DateComponents()
+    var firstWeekday : Int = 0
+    var daysCountInMonth : Int = 0
+    var weekdayAdding : Int = 0
+    var days: [String] = []
+    var isShowingCalendar : Bool = true
+    var dayoftheweek = ["월", "화", "수", "목", "금", "토", "일"]
     
-    private lazy var currentMonthLabel = UILabel().then {
+    private let previousMonthButton = UIButton()
+    
+    private let previousMonthView = UIView()
+    
+    private let previousMonthImage = UIImageView().then {
+        $0.image = UIImage(named: "previousMonth")
+    }
+    
+    private let currentDateLabelView = UIView()
+    
+    private lazy var currentDateLabel = UILabel().then {
         $0.textColor = .colorECE5FF
-        $0.font = .pretendard(.bold, size: 14)
-        $0.text = "\(currentMonth)월"
+        $0.font = .pretendard(.bold, size: 16)
+        $0.textAlignment = .center
     }
     
-    private let previousMonthButton = UIButton().then {
-        $0.setImage(UIImage(named: "previousMonth"), for: .normal)
+    private let nextMonthButton = UIButton()
+    
+    private let nextMonthView = UIView()
+    
+    private let nextMonthImage = UIImageView().then {
+        $0.image = UIImage(named: "nextMonth")
     }
     
-    private let nextMonthButton = UIButton().then {
-        $0.setImage(UIImage(named: "nextMonth"), for: .normal)
-    }
-    
-    private let stackView = UIStackView().then {
-        $0.axis = .horizontal
-    }
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [previousMonthView, currentDateLabelView, nextMonthView])
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 9
+        
+        return stackView
+    }()
     
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 0
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(CalendarCell.self, forCellWithReuseIdentifier: "CalendarCell")
+        collectionView.register(DayCell.self, forCellWithReuseIdentifier: "DayCell")
+        collectionView.register(DayOfTheWeekCell.self, forCellWithReuseIdentifier: "DayOfTheWeekCell")
         collectionView.isPagingEnabled = true
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = .color7442FF
+        collectionView.isScrollEnabled = false
         
         return collectionView
     }()
     
-    private let showOrHideDateButton = UIButton().then {
-        $0.setImage(UIImage(named: "hide"), for: .normal)
+    private let showHideCalendarButton = UIButton()
+    
+    private let showHideCalendarImage = UIImageView().then {
+        $0.image = UIImage(named: "show")
     }
     
     // MARK: - LIFECYCLE
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.addSubview(previousMonthButton)
-        self.addSubview(currentMonthLabel)
-        self.addSubview(nextMonthButton)
+        self.addSubview(previousMonthView)
+        self.addSubview(currentDateLabelView)
+        self.addSubview(nextMonthView)
+        self.addSubview(stackView)
         self.addSubview(collectionView)
-        self.addSubview(showOrHideDateButton)
+        self.addSubview(showHideCalendarButton)
+        
+        
+        previousMonthView.addSubview(previousMonthImage)
+        previousMonthView.addSubview(previousMonthButton)
+        
+        currentDateLabelView.addSubview(currentDateLabel)
+        
+        nextMonthView.addSubview(nextMonthImage)
+        nextMonthView.addSubview(nextMonthButton)
+        
+        showHideCalendarButton.addSubview(showHideCalendarImage)
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        setLayout()
         
+        setLayout()
         setAction()
+        
+        dateFormatter.dateFormat = "yyyy년 M월"
+        components.year = cal.component(.year, from: Date())
+        components.month = cal.component(.month, from: Date())
+        components.day = 1
+        calculationDate()
+        
+        self.backgroundColor = .color7442FF
+        self.layer.cornerRadius = 15
+        self.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(333).priority(1)
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -71,67 +125,185 @@ class CalendarView: BaseUIView {
     
     // MARK: - ACTIONS
     func setLayout() {
+        previousMonthView.snp.makeConstraints {
+            $0.width.height.equalTo(26)
+        }
+        
         previousMonthButton.snp.makeConstraints {
-            $0.trailing.equalTo(currentMonthLabel.snp.leading).offset(-33)
-            $0.centerY.equalTo(currentMonthLabel)
+            $0.leading.trailing.top.bottom.equalTo(previousMonthView)
+        }
+        
+        previousMonthImage.snp.makeConstraints {
+            $0.centerX.equalTo(previousMonthView)
+            $0.centerY.equalTo(previousMonthView)
             $0.width.equalTo(7.28)
             $0.height.equalTo(13)
         }
         
-        currentMonthLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().offset(50)
+        currentDateLabelView.snp.makeConstraints {
+            $0.width.equalTo(83)
+        }
+
+        currentDateLabel.snp.makeConstraints {
+            $0.leading.equalTo(currentDateLabelView)
+            $0.centerY.equalTo(currentDateLabelView)
+        }
+        
+        nextMonthView.snp.makeConstraints {
+            $0.width.height.equalTo(26)
         }
         
         nextMonthButton.snp.makeConstraints {
-            $0.leading.equalTo(currentMonthLabel.snp.trailing).offset(33)
-            $0.centerY.equalTo(currentMonthLabel)
+            $0.leading.trailing.top.bottom.equalTo(nextMonthView)
+        }
+        
+        nextMonthImage.snp.makeConstraints {
+            $0.centerX.equalTo(nextMonthView)
+            $0.centerY.equalTo(nextMonthView)
             $0.width.equalTo(7.28)
             $0.height.equalTo(13)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(50)
         }
         
         collectionView.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(21)
             $0.trailing.equalToSuperview().offset(-21)
-            $0.top.equalTo(currentMonthLabel.snp.bottom).offset(27)
-            $0.bottom.equalTo(showOrHideDateButton.snp.top).offset(-6.5)
+            $0.top.equalTo(stackView.snp.bottom).offset(18).priority(1)
+            $0.bottom.equalTo(showHideCalendarButton.snp.top).offset(-6.5)
+            //$0.height.equalTo(208)
         }
         
-        showOrHideDateButton.snp.makeConstraints {
+        showHideCalendarImage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(5)
             $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview().offset(-11)
             $0.width.equalTo(15)
             $0.height.equalTo(8)
+        }
+        
+        showHideCalendarButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.width.equalTo(35)
+            $0.height.equalTo(24)
         }
     }
     
     func setAction() {
-        previousMonthButton.addTarget(self, action: #selector( previousMonthButtonTapped), for: .touchUpInside)
+        previousMonthButton.addTarget(self, action: #selector(previousMonthButtonTapped), for: .touchUpInside)
+        nextMonthButton.addTarget(self, action: #selector(nextMonthButtonTapped), for: .touchUpInside)
+        showHideCalendarButton.addTarget(self, action: #selector(showHideCalendarButtonTapped), for: .touchUpInside)
     }
     
     @objc func previousMonthButtonTapped(sender: UIButton!) {
-        if currentMonth == 1 {
-            currentMonth = 12
+        components.month = components.month! - 1
+        calculationDate()
+        collectionView.reloadData()
+    }
+    
+    @objc func nextMonthButtonTapped(sender: UIButton!) {
+        components.month = components.month! + 1
+        calculationDate()
+        collectionView.reloadData()
+    }
+    
+    @objc func showHideCalendarButtonTapped(sender: UIButton!) {
+        if isShowingCalendar {
+            showHideCalendarImage.image = UIImage(named: "hide")
+            self.snp.makeConstraints {
+                $0.height.equalTo(159).priority(2)
+            }
+            collectionView.snp.makeConstraints {
+                $0.top.equalTo(stackView.snp.bottom).offset(5).priority(2)
+            }
+            days = ["1","2","3","4","5","6","7"]
+            
         } else {
-            currentMonth -= 1
+            showHideCalendarImage.image = UIImage(named: "show")
+            self.snp.makeConstraints {
+                $0.height.equalTo(333).priority(2)
+            }
+            collectionView.snp.makeConstraints {
+                $0.top.equalTo(stackView.snp.bottom).offset(18).priority(2)
+            }
+            calculationDate()
         }
-        currentMonthLabel.text = "\(currentMonth)월"
+       
+        collectionView.reloadData()
+        isShowingCalendar = !isShowingCalendar
+    }
+    
+    func calculationDate() {
+        self.days.removeAll()
+        components.month = components.month! - 1
+        let firstDayOfPreMonth = cal.date(from: components)
+        var lastDay = cal.range(of: .day, in: .month, for: firstDayOfPreMonth!)!.count
+        
+        components.month = components.month! + 1
+        let firstDayOfMonth = cal.date(from: components)
+        firstWeekday = cal.component(.weekday, from: firstDayOfMonth!)
+        daysCountInMonth = cal.range(of: .day, in: .month, for: firstDayOfMonth!)!.count
+        weekdayAdding = 2 - firstWeekday
+
+        self.currentDateLabel.text = dateFormatter.string(from: firstDayOfMonth!)
+        
+        for day in weekdayAdding...daysCountInMonth {
+            if day < 1 {
+                days.insert(String(lastDay), at: 0)
+                lastDay -= 1
+            } else {
+                days.append(String(day))
+            }
+        }
+        
+        var day = 1
+        while days.count % 7 != 0 {
+            days.append(String(day))
+            day += 1
+        }
     }
 }
 
 extension CalendarView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 5
+        if !isShowingCalendar {
+            return 2
+        }
+        return 1
     }
     
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 7
+        if !isShowingCalendar {
+            if section == 0 {
+                return dayoftheweek.count
+            }
+            return days.count
+        }
+        return days.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCell", for: indexPath) as? CalendarCell else { return UICollectionViewCell() }
-        
+        if !isShowingCalendar {
+            if indexPath.section ==  0 {
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DayOfTheWeekCell", for: indexPath) as? DayOfTheWeekCell else { return UICollectionViewCell() }
+                cell.dayOfTheWeekLabel.text = dayoftheweek[indexPath.row]
+                return cell
+            }
+
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DayCell", for: indexPath) as? DayCell else { return UICollectionViewCell() }
+            cell.dayLabel.text = days[indexPath.row]
+            return cell
+        }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DayCell", for: indexPath) as? DayCell else { return UICollectionViewCell() }
+        cell.dayLabel.text = days[indexPath.row]
+        if indexPath.row >= firstWeekday - 1 && indexPath.row <= daysCountInMonth + firstWeekday - 2 {
+            cell.dayLabel.textColor = .colorF3F3F3
+        } else {
+            cell.dayLabel.textColor = .colorF3F3F303
+        }
         return cell
     }
     
@@ -141,7 +313,13 @@ extension CalendarView: UICollectionViewDelegate, UICollectionViewDataSource, UI
         return 0
     }
     
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: collectionView.bounds.width / 7.0, height: collectionView.bounds.height / 7.0)
-//    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if !isShowingCalendar {
+            if indexPath.section == 0 {
+                return CGSize(width: collectionView.bounds.width / 7.0, height: 20)
+            }
+            return CGSize(width: collectionView.bounds.width / 7.0, height: 29)
+        }
+        return CGSize(width: collectionView.bounds.width / 7.0, height: 208 / CGFloat(days.count / 7))
+    }
 }
--- WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/TestViewController.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/TestViewController.swift
@@ -8,11 +8,7 @@
 import UIKit
 
 class TestViewController: BaseViewController {
-    private let calendarView = CalendarView().then {
-        $0.backgroundColor = .color7442FF
-        $0.layer.cornerRadius = 15
-        $0.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-    }
+    private let calendarView = CalendarView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +21,6 @@ class TestViewController: BaseViewController {
         calendarView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            $0.height.equalTo(333)
         }
     }
 }
