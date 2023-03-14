2f2b66572a0ae0fa9bf3d63b63b0c2e8600f5334
ryuchanghwi <ckdgnl10@naver.com>
2023-03-15 00:38:10 +0900

    #23 - [Feature] NavigationController 구현

--- WorkoutDone/WorkoutDone/Sources/Classes/TabBar/TabBarController.swift
+++ WorkoutDone/WorkoutDone/Sources/Classes/TabBar/TabBarController.swift
@@ -11,17 +11,23 @@ class TabBarController : UITabBarController {
     let homeTab = UINavigationController(rootViewController: HomeViewController())
     let routineTab = UINavigationController(rootViewController: RoutineViewController())
     let myRecordTab = UINavigationController(rootViewController: MyRecordViewController())
-    let homeTabBarItem = UITabBarItem(title: "오늘의 운동", image: UIImage(named: ""), tag: 0)
-    let routineTabBarItem = UITabBarItem(title: "운동 루틴", image: UIImage(named: ""), tag: 1)
-    let myRecordTabBarItem = UITabBarItem(title: "나의 기록", image: UIImage(named: ""), tag: 2)
+    let homeTabBarItem = UITabBarItem(title: "오늘의 운동", image: UIImage(systemName: "mic"), tag: 0)
+    let routineTabBarItem = UITabBarItem(title: "운동 루틴", image: UIImage(systemName: "mic"), tag: 1)
+    let myRecordTabBarItem = UITabBarItem(title: "나의 기록", image: UIImage(systemName: "mic"), tag: 2)
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeTab.tabBarItem = homeTabBarItem
+        routineTab.tabBarItem = routineTabBarItem
+        myRecordTab.tabBarItem = myRecordTabBarItem
         viewControllers = [homeTab, routineTab, myRecordTab]
         tabBar.isTranslucent = false
-        tabBar.barTintColor = .purple
+        tabBar.barTintColor = .colorFFFFFF
         let appearance = UITabBarAppearance()
         appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = UIColor.blue
+//        appearance.backgroundColor = UIColor.blue
+        
+        tabBar.tintColor = .color000000
+        tabBar.unselectedItemTintColor = .lightGray
         
         //Todo: - 텝바 색상, 탭바 이미지, 텍스트 정하기
     }
---
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Workout/WorkoutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WorkoutViewController.swift
+//  WorkoutDone
+//
+//  Created by 류창휘 on 2023/03/14.
+//
+
+import UIKit
+
+class WorkoutViewController : BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
