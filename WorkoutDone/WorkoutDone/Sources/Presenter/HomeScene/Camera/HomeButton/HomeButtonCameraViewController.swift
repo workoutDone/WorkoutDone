3b1e36a3fd6f6dada1760880ddc272d7977ec280
hyemib <hyem0127@gmail.com>
2023-03-22 21:55:35 +0900

    #38 [Style] HomeButtonCameraViewController UI 구성

---
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Camera/HomeButton/HomeButtonCameraViewController.swift
@@ -0,0 +1,143 @@
+//
+//  HomeButtonCameraViewController.swift
+//  WorkoutDone
+//
+//  Created by hyemi on 2023/03/17.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class HomeButtonCameraViewController : BaseViewController {
+    private let cameraView = UIView().then {
+        $0.backgroundColor = .yellow
+    }
+    
+    private let collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(FrameCell.self, forCellWithReuseIdentifier: "FrameCell")
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        return collectionView
+    }()
+    
+    private let shutterButton = UIButton().then {
+        $0.setImage(UIImage(named: "shutter"), for: .normal)
+    }
+    
+    private let switchCameraButton = UIButton().then {
+        $0.setImage(UIImage(named: "switchCamera"), for: .normal)
+    }
+    
+    private let pressShutterView = PressShutterView()
+    
+//    private let saveButton = GradientButton(colors: [UIColor.color8E36FF.cgColor, UIColor.color7442FF.cgColor]).then {
+//           $0.setTitle("저장하기", for: .normal)
+//           $0.titleLabel?.font = .pretendard(.bold, size: 20)
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setDelegateDataSource()
+        pressShutterView.isHidden = true
+//        saveButton.isHidden = true
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        [cameraView, collectionView, shutterButton, switchCameraButton, pressShutterView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setupConstraints() {
+        super.setupConstraints()
+        
+        cameraView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(468)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(cameraView.snp.bottom).offset(12)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(66)
+        }
+        
+        shutterButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
+            $0.width.height.equalTo(50)
+        }
+        
+        switchCameraButton.snp.makeConstraints {
+            $0.bottom.equalTo(shutterButton)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-17)
+            $0.width.height.equalTo(42)
+        }
+        
+        pressShutterView.snp.makeConstraints {
+            $0.top.equalTo(cameraView.snp.bottom)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+        
+//        saveButton.snp.makeConstraints {
+//            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-13)
+//            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+//            $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
+//            $0.height.equalTo(58)
+//        }
+    }
+    
+    override func actions() {
+        shutterButton.addTarget(self, action: #selector(captureButtonTapped), for: .touchUpInside)
+        switchCameraButton.addTarget(self, action: #selector(switchCameraButtonTapped), for: .touchUpInside)
+    }
+    
+    func setDelegateDataSource() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    @objc func captureButtonTapped(sender: UIButton!) {
+        collectionView.isHidden = true
+        shutterButton.isHidden = true
+        switchCameraButton.isHidden = true
+        pressShutterView.isHidden = false
+    }
+    
+    @objc func switchCameraButtonTapped(sender: UIButton!) {
+        print(2)
+    }
+    
+}
+
+extension HomeButtonCameraViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameCell", for: indexPath) as? FrameCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 66 , height: 66)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 10
+    }
+}
