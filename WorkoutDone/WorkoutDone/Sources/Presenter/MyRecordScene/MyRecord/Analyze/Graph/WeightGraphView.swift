bbe7105eb1c435870cb4cff70d7299f579a2c4aa
ryuchanghwi <ckdgnl10@naver.com>
2023-03-30 01:09:58 +0900

    #65 - [Feature] WeightGraphView 스크롤 우측 정렬 구현

--- WorkoutDone/WorkoutDone/Sources/Presenter/MyRecordScene/MyRecord/Analyze/Graph/WeightGraphView.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/MyRecordScene/MyRecord/Analyze/Graph/WeightGraphView.swift
@@ -55,6 +55,7 @@ var list = [
 ]
 
 struct WeightGraphView: View {
+    @Namespace var trailingID
     ///DateToString
     func formatDate(_ date : Date) -> String {
         let cal = Calendar.current
@@ -78,82 +79,92 @@ struct WeightGraphView: View {
     ///ViewAppear 시 애니메이션 사용 위한 변수
     @State private var animate : Bool = false
     @State private var plotWidth: CGFloat = 0
-    
+    ///데이터의 최댓값
+
     var body: some View {
         let max = testData.max { item1, item2 in
             return item2.weight > item1.weight
         }?.weight ?? 0
-        ScrollView(.horizontal) {
-            Chart(testData, id: \.date) { data in
-                LineMark(
-                    x: .value("Month", data.date.toString()),
-                    y: .value("Weight", animate ? data.weight : 0)
-                )
-                .interpolationMethod(.cardinal)
-                .foregroundStyle(Color(UIColor.color7442FF))
-                PointMark(
-                    x: .value("Month", data.date.toString()),
-                    y: .value("Weight", animate ? data.weight : 0)
-                )
-                ///custom point
-                .annotation(position: .overlay, alignment: .center) {
-                    ZStack {
-                        Circle()
-                            .frame(width: 12, height: 12)
-                            .foregroundColor(Color(UIColor.color7442FF))
-                        Circle()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(Color(UIColor.colorFFFFFF))
-                    }
-                    .shadow(color: Color(UIColor.color7442FF), radius: 2)
-                }
-                if let currentActiveItem, currentActiveItem.date == data.date {
-                    RuleMark(x: .value("Month", data.date.toString()))
-                        .foregroundStyle(Color(UIColor.color7442FF))
-                        .lineStyle(.init(lineWidth: 1, lineCap: .round, miterLimit: 2, dash: [2], dashPhase: 5))
-                        .annotation(position: .top) {
-                            ZStack {
-                                Image("speechBubble")
-                                    .resizable()
-                                    .frame(width: 50, height: 42)
-                                    .offset(y: 6)
-                                Text("\(Int(currentActiveItem.weight))kg")
-                                    .foregroundColor(Color(UIColor.color7442FF))
-                                    .font(Font(UIFont.pretendard(.semiBold, size: 14)))
-                            }
-                            .offset(x: 0, y: max - data.weight + 40)
+        ///우측 정렬을 위한 scrollViewReader
+        ScrollViewReader { proxy in
+            ScrollView(.horizontal) {
+                Chart(testData, id: \.date) { data in
+                    LineMark(
+                        x: .value("Month", data.date.toString()),
+                        y: .value("Weight", animate ? data.weight : 0)
+                    )
+                    .interpolationMethod(.cardinal)
+                    .foregroundStyle(Color(UIColor.color7442FF))
+                    PointMark(
+                        x: .value("Month", data.date.toString()),
+                        y: .value("Weight", animate ? data.weight : 0)
+                    )
+                    ///custom point
+                    .annotation(position: .overlay, alignment: .center) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(Color(UIColor.color7442FF))
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(Color(UIColor.colorFFFFFF))
                         }
+                        .shadow(color: Color(UIColor.color7442FF), radius: 2)
+                    }
+                    if let currentActiveItem, currentActiveItem.date == data.date {
+                        RuleMark(x: .value("Month", data.date.toString()))
+                            .foregroundStyle(Color(UIColor.color7442FF))
+                            .lineStyle(.init(lineWidth: 1, lineCap: .round, miterLimit: 2, dash: [2], dashPhase: 5))
+                            .annotation(position: .top) {
+                                ZStack {
+                                    Image("speechBubble")
+                                        .resizable()
+                                        .frame(width: 50, height: 42)
+                                        .offset(y: 6)
+                                    Text("\(Int(currentActiveItem.weight))kg")
+                                        .foregroundColor(Color(UIColor.color7442FF))
+                                        .font(Font(UIFont.pretendard(.semiBold, size: 14)))
+                                }
+                                .offset(x: 0, y: max - data.weight + 40)
+                            }
+                    }
                 }
+                
+    //            .chartXAxis {
+    //                AxisMarks(preset: <#T##AxisMarkPreset#>)
+    //            }
+                .chartYAxis {
+                    AxisMarks(position: .trailing)
+                }
+                .chartYScale(domain: 0...(max + 50))
+                .chartOverlay(content: { proxy in
+                    GeometryReader { innerProxy in
+                        Rectangle()
+                            .fill(.clear).contentShape(Rectangle())
+                            .onTapGesture { value in
+                                if let date : String = proxy.value(atX: value.x) {
+                                    if let currentItem = testData.first(where: { item in
+                                        item.date.toString() == date
+                                    }) {
+                                        print(currentItem.weight, "sssdd")
+                                        self.currentActiveItem = currentItem
+                                        self.plotWidth = proxy.plotAreaSize.width
+                                    }
+
+                                }
+                            }
+                    }
+                })
+                .padding()
+                .frame(width: ViewConstants.dataPointWidth * CGFloat(testData.count))
+                //우측 정렬을 위한 id 설졍
+                .id(trailingID)
+            }
+            .onAppear {
+                //우측 정렬을 위한 anchor 설정
+                proxy.scrollTo(trailingID, anchor: .trailing)
             }
             
-//            .chartXAxis {
-//                AxisMarks(preset: <#T##AxisMarkPreset#>)
-//            }
-            .chartYAxis {
-                AxisMarks(position: .trailing)
-            }
-            .chartYScale(domain: 0...(max + 50))
-            .chartOverlay(content: { proxy in
-                GeometryReader { innerProxy in
-                    Rectangle()
-                        .fill(.clear).contentShape(Rectangle())
-                        .onTapGesture { value in
-                            if let date : String = proxy.value(atX: value.x) {
-                                print(date)
-                                if let currentItem = testData.first(where: { item in
-                                    item.date.toString() == date
-                                }) {
-                                    print(currentItem.weight, "sssdd")
-                                    self.currentActiveItem = currentItem
-                                    self.plotWidth = proxy.plotAreaSize.width
-                                }
-
-                            }
-                        }
-                }
-            })
-            .padding()
-            .frame(width: ViewConstants.dataPointWidth * CGFloat(testData.count))
         }
         .background {
             RoundedRectangle(cornerRadius: 15, style: .continuous)
