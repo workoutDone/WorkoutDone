7179783f5dd22198d071e363d6c3273eeee6fe38
ryuchanghwi <ckdgnl10@naver.com>
2023-03-30 19:29:20 +0900

    #65 - [Feature] WeightGraphView 데이터 갯수에 따른 UI 에러 해결

--- WorkoutDone/WorkoutDone/Sources/Presenter/MyRecordScene/MyRecord/Analyze/Graph/WeightGraphView.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/MyRecordScene/MyRecord/Analyze/Graph/WeightGraphView.swift
@@ -5,6 +5,8 @@
 //  Created by 류창휘 on 2023/03/26.
 //
 
+//MARK: - TODO 갯수에 따른 스크롤 여부 에러 구현
+
 import SwiftUI
 import Charts
 
@@ -32,25 +34,25 @@ struct TestModel {
     let weight : Double
 }
 var list = [
-    TestModel(date: "2023.01.01".toDate()!, weight: 0),
+    TestModel(date: "2023.01.01".toDate()!, weight: 1),
     TestModel(date: "2023.01.02".toDate()!, weight: 73),
-    TestModel(date: "2023.01.03".toDate()!, weight: 74),
-    TestModel(date: "2023.01.05".toDate()!, weight: 71),
-    TestModel(date: "2023.01.06".toDate()!, weight: 60),
-    TestModel(date: "2023.01.07".toDate()!, weight: 80),
-    TestModel(date: "2023.01.14".toDate()!, weight: 80),
-    TestModel(date: "2023.01.15".toDate()!, weight: 50),
-    TestModel(date: "2023.01.29".toDate()!, weight: 60),
-    TestModel(date: "2023.02.01".toDate()!, weight: 70),
-    TestModel(date: "2023.02.02".toDate()!, weight: 75),
-    TestModel(date: "2023.02.03".toDate()!, weight: 75),
-    TestModel(date: "2023.02.05".toDate()!, weight: 75),
-    TestModel(date: "2023.02.06".toDate()!, weight: 75),
-    TestModel(date: "2023.02.07".toDate()!, weight: 75),
-    TestModel(date: "2023.02.08".toDate()!, weight: 75),
-    TestModel(date: "2023.02.09".toDate()!, weight: 75),
-    TestModel(date: "2023.02.10".toDate()!, weight: 75),
-    TestModel(date: "2023.02.11".toDate()!, weight: 100),
+//    TestModel(date: "2023.01.03".toDate()!, weight: 74),
+//    TestModel(date: "2023.01.05".toDate()!, weight: 71),
+//    TestModel(date: "2023.01.06".toDate()!, weight: 60),
+//    TestModel(date: "2023.01.07".toDate()!, weight: 80),
+//    TestModel(date: "2023.01.14".toDate()!, weight: 80),
+//    TestModel(date: "2023.01.15".toDate()!, weight: 50),
+//    TestModel(date: "2023.01.29".toDate()!, weight: 60),
+//    TestModel(date: "2023.02.01".toDate()!, weight: 70),
+//    TestModel(date: "2023.02.02".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.03".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.05".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.06".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.07".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.08".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.09".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.10".toDate()!, weight: 75),
+//    TestModel(date: "2023.02.11".toDate()!, weight: 150),
     
 ]
 
@@ -85,6 +87,9 @@ struct WeightGraphView: View {
         let max = testData.max { item1, item2 in
             return item2.weight > item1.weight
         }?.weight ?? 0
+        let min = testData.min { item1, item2 in
+            return item2.weight > item1.weight
+        }?.weight ?? 0
         ///우측 정렬을 위한 scrollViewReader
         ScrollViewReader { proxy in
             ScrollView(.horizontal) {
@@ -99,7 +104,7 @@ struct WeightGraphView: View {
                         x: .value("Month", data.date.toString()),
                         y: .value("Weight", animate ? data.weight : 0)
                     )
-                    ///custom point
+                    ///커스텀 포인트 마크
                     .annotation(position: .overlay, alignment: .center) {
                         ZStack {
                             Circle()
@@ -125,7 +130,11 @@ struct WeightGraphView: View {
                                         .foregroundColor(Color(UIColor.color7442FF))
                                         .font(Font(UIFont.pretendard(.semiBold, size: 14)))
                                 }
-                                .offset(x: 0, y: max - data.weight + 40)
+                                .offset(x: 0, y: -data.weight + 200 - 20)
+//                                .offset(x: 0, y: (max / min > 100 ? -(200 - data.weight) : max - data.weight + 40 ))
+//                                .offset(x: 0, y: -data.weight + max + 40)
+//                                .offset(x: 0, y: (max - data.weight + 40))
+//                                .offset(x: 0, y: 220 - data.weight - max) // + 밑, - 위
                             }
                     }
                 }
@@ -136,7 +145,7 @@ struct WeightGraphView: View {
                 .chartYAxis {
                     AxisMarks(position: .trailing)
                 }
-                .chartYScale(domain: 0...(max + 50))
+                .chartYScale(domain: max > 100 ? 0...(max + 100) : 0...(max + 40))
                 .chartOverlay(content: { proxy in
                     GeometryReader { innerProxy in
                         Rectangle()
@@ -156,12 +165,13 @@ struct WeightGraphView: View {
                     }
                 })
                 .padding()
-                .frame(width: ViewConstants.dataPointWidth * CGFloat(testData.count))
-                //우측 정렬을 위한 id 설졍
+                ///데이터 갯수에 따른 차트 UI 구분
+                .frame(width: UIScreen.main.bounds.width > ViewConstants.dataPointWidth * CGFloat(testData.count) ? UIScreen.main.bounds.width : ViewConstants.dataPointWidth * CGFloat(testData.count))
+                ///우측 정렬을 위한 id 설졍
                 .id(trailingID)
             }
             .onAppear {
-                //우측 정렬을 위한 anchor 설정
+                ///우측 정렬을 위한 anchor 설정
                 proxy.scrollTo(trailingID, anchor: .trailing)
             }
             
