51b700cad921a786d6d778438a509b6adff1383d
ryuchanghwi <ckdgnl10@naver.com>
2023-04-18 02:33:09 +0900

    #104, #105 - [Feature] extensions 코드추가, rx ViewModelType protocol 추가

---
+++ WorkoutDone/WorkoutDone/Sources/Extensions/UIStackView.swift
@@ -0,0 +1,15 @@
+//
+//  UIStackView.swift
+//  WorkoutDone
+//
+//  Created by 류창휘 on 2023/04/18.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach { self.addArrangedSubviews($0) }
+    }
+}
