17c3f71c9ea90e9b61f1939016a0ef0338d164d7
ryuchanghwi <ckdgnl10@naver.com>
2023-05-09 13:22:35 +0900

    NotificationName Extensions 생성

---
+++ WorkoutDone/WorkoutDone/Sources/Extensions/NotificationName.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationCenterName.swift
+//  WorkoutDone
+//
+//  Created by 류창휘 on 2023/05/09.
+//
+
+import UIKit
+
+extension Notification.Name {
+    ///선택한 날짜
+    static let selectedDateInt = Notification.Name("selectedDate")
+}
