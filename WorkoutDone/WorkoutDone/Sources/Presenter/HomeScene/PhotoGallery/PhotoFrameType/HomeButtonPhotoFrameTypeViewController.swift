7cc93d4e63662085f3a4c14ca898ce446efb4b6b
ryuchanghwi <ckdgnl10@naver.com>
2023-05-12 17:02:08 +0900

    #157 - [Style] HomeButtonPhotoFrameTypeViewController 구현

--- WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/PhotoGallery/PhotoFrameType/HomeButtonPhotoFrameTypeViewController.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/PhotoGallery/PhotoFrameType/HomeButtonPhotoFrameTypeViewController.swift
@@ -6,17 +6,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class HomeButtonPhotoFrameTypeViewController : BaseViewController {
+    typealias frame = PhotoFrameType
+    var selectedImage : UIImage?
+    
+    private var viewModel = PhotoFrameTypeViewModel()
+    
+    
+    private var saveData = PublishSubject<Void>()
+    private var selectedFrameType = PublishSubject<Int>()
+    private var selectedFrameTypeButtonStatus = BehaviorSubject(value: false)
+    private var selectedPhoto = PublishSubject<UIImage>()
+    private var selectedDate = PublishSubject<Int>()
+    
+    private lazy var input = PhotoFrameTypeViewModel.Input(
+        frameTypeButtonStatus: selectedFrameTypeButtonStatus.asDriver(onErrorJustReturn: false),
+        selectedFrameType: selectedFrameType.asDriver(onErrorJustReturn: 0),
+        selectedPhoto: selectedPhoto.asDriver(onErrorJustReturn: UIImage()),
+        selectedDate: selectedDate.asDriver(onErrorJustReturn: 0),
+        saveButtonTapped: saveData.asDriver(onErrorJustReturn: ()))
+    private lazy var output = viewModel.transform(input: input)
+    
+    
     // MARK: - PROPERTIES
     private let saveButton = GradientButton(colors: [UIColor.color7442FF.cgColor, UIColor.color8E36FF.cgColor]).then {
         $0.setTitle("저장하기", for: .normal)
         $0.titleLabel?.font = .pretendard(.bold, size: 20)
         $0.setTitleColor(UIColor.colorFFFFFF, for: .normal)
     }
-    private let backButton = BackButton()
+    private var disEnabledSaveButton = UIButton().then {
+        $0.setTitle("저장하기", for: .normal)
+        $0.titleLabel?.font = .pretendard(.bold, size: 20)
+        $0.layer.cornerRadius = 12
+        $0.backgroundColor = .colorCCCCCC
+        $0.setTitleColor(UIColor.colorFFFFFF, for: .normal)
+    }
     
-    private let selectedImageView = UIImageView()
+    private let selectedImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
     private let photoFrameBackView = UIView()
     
     private let saveButtonAreaBackView = UIView()
@@ -73,18 +104,68 @@ class HomeButtonPhotoFrameTypeViewController : BaseViewController {
     // MARK: - LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
+        setImage()
 
+    }
+    override func setupBinding() {
+        output.saveData.drive(onNext: {
+            self.navigationController?.popToRootViewController(animated: true)
+        })
+        .disposed(by: disposeBag)
+        
+        output.saveButtonStatus.drive(onNext: { value in
+            if value {
+                self.saveButton.isHidden = false
+                self.disEnabledSaveButton.isHidden = true
+                print("활성")
+            }
+            else {
+                self.saveButton.isHidden = true
+                self.disEnabledSaveButton.isHidden = false
+                print("비활")
+            }
+        })
+        .disposed(by: disposeBag)
+        
+        
+        
+        saveButton.rx.tap
+            .bind { value in
+                self.saveData.onNext(())
+                print("눌려따")
+            }
+            .disposed(by: disposeBag)
+        
+
+        
+        guard let image = selectedImage else { return }
+        let resizedImage = resizeImage(image: image, newSize: CGSize(width: view.frame.width, height: view.frame.width * 4 / 3))
+        selectedPhoto.onNext(resizedImage)
+        guard let homeVC = self.navigationController?.viewControllers.first as? HomeViewController else { return }
+        let homeVCDate = homeVC.calendarView.selectDate ?? Date()
+        selectedDate.onNext(homeVCDate.dateToInt())
     }
     
     override func setComponents() {
         super.setComponents()
         view.backgroundColor = .colorFFFFFF
         
+        [defaultFrameButton, manFirstUpperBodyFrameButton, manSecondUpperBodyFrameButton, manWholeBodyFrameButton, womanFirstUpperBodyFrameButton, womanSecondUpperBodyFrameButton, womanWholeBodyFrameButton].forEach {
+            frameButtons.append($0)
+        }
+        defaultFrameButton.tag = frame.defaultFrame.getFrameType()
+        manFirstUpperBodyFrameButton.tag = frame.manFirstUpperBodyFrame.getFrameType()
+        manSecondUpperBodyFrameButton.tag = frame.manSecondUpperBodyFrame.getFrameType()
+        manWholeBodyFrameButton.tag = frame.manWholeBodyFrame.getFrameType()
+        womanFirstUpperBodyFrameButton.tag = frame.womanFirstUpperBodyFrame.getFrameType()
+        womanSecondUpperBodyFrameButton.tag = frame.womanSecondUpperBodyFrame.getFrameType()
+        womanWholeBodyFrameButton.tag = frame.womanWholeBodyFrame.getFrameType()
+        
     }
     override func setupLayout() {
         super.setupLayout()
         view.addSubviews(selectedImageView, photoFrameBackView, saveButtonAreaBackView)
-        saveButtonAreaBackView.addSubviews(saveButton)
+        saveButtonAreaBackView.addSubviews(saveButton, disEnabledSaveButton)
         photoFrameBackView.addSubview(photoFrameButtonsScrollView)
         photoFrameButtonsScrollView.addSubview(photoFrameButtonsScrollContentView)
         photoFrameButtonsScrollContentView.addSubview(photoFrameButtonsStackView)
@@ -113,6 +194,11 @@ class HomeButtonPhotoFrameTypeViewController : BaseViewController {
         saveButton.snp.makeConstraints {
             $0.centerY.centerX.equalToSuperview()
             $0.height.equalTo(58)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        disEnabledSaveButton.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.height.equalTo(58)
             $0.leading.trailing.equalToSuperview().inset(24)
         }
         
@@ -126,7 +212,7 @@ class HomeButtonPhotoFrameTypeViewController : BaseViewController {
         }
         photoFrameButtonsStackView.snp.makeConstraints {
             $0.top.bottom.equalToSuperview()
-            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.leading.trailing.equalToSuperview().inset(20)
         }
         
         defaultFrameButton.snp.makeConstraints {
@@ -151,11 +237,97 @@ class HomeButtonPhotoFrameTypeViewController : BaseViewController {
             $0.height.width.equalTo(66)
         }
     }
+    func setImage() {
+        if let image = selectedImage {
+            selectedImageView.image = image
+        }
+    }
     
     // MARK: - ACTIONS
+    override func actions() {
+        super.actions()
+        frameButtons.forEach {
+            $0.addTarget(self, action: #selector(frameButtonTapped(sender: )), for: .touchUpInside)
+        }
+    }
+    @objc func frameButtonTapped(sender : UIButton) {
+        for button in frameButtons {
+            if button == sender {
+//                button.isSelected = !button.isSelected
+                selectedFrameTypeButtonStatus.onNext(true)
+                button.layer.borderColor = UIColor.color7442FF.cgColor
+                button.layer.borderWidth = 2
+                
+            }
+            else {
+                button.layer.borderColor = .none
+                button.layer.borderWidth = 0
+            }
+        }
+        switch sender.tag {
+        case frame.defaultFrame.rawValue:
+            print(frame.defaultFrame.getFrameType())
+            self.selectedFrameType.onNext(frame.defaultFrame.getFrameType())
+        case frame.manFirstUpperBodyFrame.rawValue:
+            print(frame.manFirstUpperBodyFrame.getFrameType())
+            self.selectedFrameType.onNext(frame.manFirstUpperBodyFrame.getFrameType())
+        case frame.manSecondUpperBodyFrame.rawValue:
+            print(frame.manSecondUpperBodyFrame.getFrameType())
+            self.selectedFrameType.onNext(frame.manSecondUpperBodyFrame.getFrameType())
+        case frame.manWholeBodyFrame.rawValue:
+            print(frame.manWholeBodyFrame.getFrameType())
+            self.selectedFrameType.onNext(frame.manWholeBodyFrame.getFrameType())
+        case frame.womanFirstUpperBodyFrame.rawValue:
+            print(frame.womanFirstUpperBodyFrame.getFrameType())
+            self.selectedFrameType.onNext(frame.womanFirstUpperBodyFrame.getFrameType())
+        case frame.womanSecondUpperBodyFrame.rawValue:
+            print(frame.womanSecondUpperBodyFrame.getFrameType())
+            self.selectedFrameType.onNext(frame.womanSecondUpperBodyFrame.getFrameType())
+        case frame.womanWholeBodyFrame.rawValue:
+            print(frame.womanWholeBodyFrame.getFrameType())
+            self.selectedFrameType.onNext(frame.womanWholeBodyFrame.getFrameType())
+            
+        default:
+            print("?/")
+        }
+    }
+    func resizeImage(image: UIImage, newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
 }
 // MARK: - EXTENSIONs
 extension HomeButtonPhotoFrameTypeViewController {
-    
+    enum PhotoFrameType : Int {
+        case defaultFrame = 0
+        case manFirstUpperBodyFrame = 1
+        case manSecondUpperBodyFrame = 2
+        case manWholeBodyFrame = 3
+        case womanFirstUpperBodyFrame = 4
+        case womanSecondUpperBodyFrame = 5
+        case womanWholeBodyFrame = 6
+        
+        func getFrameType() -> Int {
+            switch self {
+            case .defaultFrame:
+                return PhotoFrameType.defaultFrame.rawValue
+            case .manFirstUpperBodyFrame:
+                return PhotoFrameType.manFirstUpperBodyFrame.rawValue
+            case .manSecondUpperBodyFrame:
+                return PhotoFrameType.manSecondUpperBodyFrame.rawValue
+            case .manWholeBodyFrame:
+                return PhotoFrameType.manWholeBodyFrame.rawValue
+            case .womanFirstUpperBodyFrame:
+                return PhotoFrameType.womanFirstUpperBodyFrame.rawValue
+            case .womanSecondUpperBodyFrame:
+                return PhotoFrameType.womanSecondUpperBodyFrame.rawValue
+            case .womanWholeBodyFrame:
+                return PhotoFrameType.womanWholeBodyFrame.rawValue
+            }
+        }
+    }
 }
 
