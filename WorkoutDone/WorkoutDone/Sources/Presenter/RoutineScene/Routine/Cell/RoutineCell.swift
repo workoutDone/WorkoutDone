4095213f6084c9a640d5e59aef02583ea1b3a118
hyemib <hyem0127@gmail.com>
2023-05-20 23:15:00 +0900

    #174 - [Feature] 나의 운동 루틴 리스트 뷰 구현

--- WorkoutDone/WorkoutDone/Sources/Presenter/RoutineScene/Routine/Cell/RoutineCell.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/RoutineScene/Routine/Cell/RoutineCell.swift
@@ -9,64 +9,71 @@ import UIKit
 import SnapKit
 import Then
 
-class RoutineCell : UICollectionViewCell {
-    private let bodyPartView = UIView().then {
-        $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.color929292.cgColor
-        $0.layer.cornerRadius = 23 / 2
+class RoutineCell : UITableViewCell {
+    var seletedIndex = -1
+    
+    private let routineIndexLabel = UILabel().then {
+        $0.text = "routine A"
+        $0.textColor = .color7442FF
+        $0.font = .pretendard(.regular, size: 12)
     }
     
-    private let bodyPartLabel = UILabel().then {
-        $0.text = "어깨"
-        $0.textColor = .color929292
-        $0.font = .pretendard(.regular, size: 14)
+    let routineTitleLabel = UILabel().then {
+        $0.text = "오늘은 등 운동!"
+        $0.textColor = .color121212
+        $0.font = .pretendard(.semiBold, size: 16)
     }
     
-    private let weightTrainingLabel = UILabel().then {
-        $0.text = "배틀 로프"
-        $0.font = .pretendard(.regular, size: 16)
+    let editButton = UIButton().then {
+        $0.layer.cornerRadius = 5
+        $0.backgroundColor = .colorF3F3F3
+        $0.setTitle("수정하기", for: .normal)
+        $0.setTitleColor(UIColor.color363636, for: .normal)
+        $0.titleLabel?.font = .pretendard(.semiBold, size: 14)
+        $0.isHidden = true
     }
     
     // MARK: - LIFECYCLE
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        backgroundColor = .colorF3F3F3
-        layer.cornerRadius = 8
-        
-        setupLayout()
-        setupConstraints()
-        
-        
+         setupLayout()
+         setupConstraints()
     }
-    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+            
+    }
+    
     // MARK: - ACTIONS
     func setupLayout() {
-        contentView.addSubview(bodyPartView)
-        bodyPartView.addSubview(bodyPartLabel)
-        
-        contentView.addSubview(weightTrainingLabel)
+        self.addSubview(routineIndexLabel)
+        self.addSubview(routineTitleLabel)
+        self.addSubview(editButton)
     }
     
     func setupConstraints() {
-        bodyPartView.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(10)
-            $0.centerY.equalToSuperview()
-            $0.width.equalTo(37)
-            $0.height.equalTo(23)
+        routineIndexLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(11)
+            $0.leading.equalToSuperview().offset(39)
         }
         
-        bodyPartLabel.snp.makeConstraints {
-            $0.centerX.centerY.equalTo(bodyPartView)
+        routineTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(routineIndexLabel.snp.bottom)
+            $0.leading.equalToSuperview().offset(39)
         }
         
-        weightTrainingLabel.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
+        editButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.width.equalTo(65)
+            $0.height.equalTo(27)
         }
     }
 }
