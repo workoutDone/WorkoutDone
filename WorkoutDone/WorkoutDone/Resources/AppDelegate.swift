bcd371440946a2b12c028f6ae2147418dba5f7c2
ryuchanghwi <ckdgnl10@naver.com>
2023-06-26 21:40:04 +0900

    홈버튼 없는 카메라 인스타그램 스토리 공유 구현

--- WorkoutDone/WorkoutDone/Resources/AppDelegate.swift
+++ WorkoutDone/WorkoutDone/Resources/AppDelegate.swift
@@ -8,37 +8,6 @@
 import UIKit
 import RealmSwift
 import NotificationCenter
-import FacebookShare
-import FacebookCore
-
-
-//@UIApplicationMain
-//class AppDelegate: UIResponder, UIApplicationDelegate {
-//func application(
-//    _ application: UIApplication,
-//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//) -> Bool {
-//    ApplicationDelegate.shared.application(
-//        application,
-//        didFinishLaunchingWithOptions: launchOptions
-//    )
-//
-//    return true
-//}
-      
-//func application(
-//    _ app: UIApplication,
-//    open url: URL,
-//    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-//) -> Bool {
-//    ApplicationDelegate.shared.application(
-//        app,
-//        open: url,
-//        sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-//        annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-//    )
-//}
-//}
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -47,10 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         let realm = try! Realm()
         print(Realm.Configuration.defaultConfiguration.fileURL)
-        ApplicationDelegate.shared.application(
-            application,
-            didFinishLaunchingWithOptions: launchOptions
-        )
         return true
     }
 
@@ -61,18 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-    func application(
-        _ app: UIApplication,
-        open url: URL,
-        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-        ApplicationDelegate.shared.application(
-            app,
-            open: url,
-            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-        )
     }
 
 
