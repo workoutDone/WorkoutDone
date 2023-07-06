ecc0cf2a60c026ad1ef8c793079ef7da75d95258
ryuchanghwi <ckdgnl10@naver.com>
2023-07-06 16:31:25 +0900

    DeleteRecordAlertViewModel 구현

--- WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/WorkoutResutl/DeleteRecordAlertViewModel.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/WorkoutResutl/DeleteRecordAlertViewModel.swift
@@ -5,4 +5,45 @@
 //  Created by 류창휘 on 2023/07/06.
 //
 
-import Foundation
+import UIKit
+import RealmSwift
+import RxSwift
+import RxCocoa
+
+class DeleteRecordAlertViewModel {
+    let realm = try! Realm()
+    let realmManager = RealmManager.shared
+    var workOutDoneData : Results<WorkOutDoneData>?
+    init(workOutDoneData: Results<WorkOutDoneData>? = nil) {
+        self.workOutDoneData = realm.objects(WorkOutDoneData.self)
+    }
+    
+    func readWorkoutDoneData(id : Int) -> WorkOutDoneData?  {
+        let workoutDoneData = RealmManager.shared.readData(id: id, type: WorkOutDoneData.self)
+        return workoutDoneData
+    }
+    
+    func deleteRoutineData(id : Int) {
+        guard let workoutDoneData = self.readWorkoutDoneData(id: id) else { return }
+        if let routine = workoutDoneData.routine {
+            realmManager.deleteData(routine)
+        }
+    }
+    
+    struct Input {
+        let deleteTrigger : Driver<Void>
+        let selectedDate : Driver<Int>
+    }
+    
+    struct Output {
+        let deleteRoutineData : Driver<Bool>
+    }
+    
+    func transform(input: Input) -> Output {
+        let deleteRoutineData = Driver<Bool>.combineLatest(input.selectedDate, input.deleteTrigger, resultSelector: { (id, _) in
+            self.deleteRoutineData(id: id)
+            return true
+        })
+        return Output(deleteRoutineData: deleteRoutineData)
+    }
+}
