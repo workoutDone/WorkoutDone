64b758851ae4e3446dd3dde065257fc428b72afb
ryuchanghwi <ckdgnl10@naver.com>
2023-07-07 17:10:55 +0900

    #364 - [Style] HomeVC 운동 결과 View UI 구현

--- WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/HomeView/WorkoutResultView.swift
+++ WorkoutDone/WorkoutDone/Sources/Presenter/HomeScene/Home/HomeView/WorkoutResultView.swift
@@ -9,7 +9,7 @@ import UIKit
 import Then
 import SnapKit
 
-class WorkoutResultView : BaseUIView {
+final class WorkoutResultView : BaseUIView {
     // MARK: - PROPERTIES
     private let workoutResultLabel = UILabel().then {
         $0.text = "운동 결과"
@@ -20,22 +20,40 @@ class WorkoutResultView : BaseUIView {
         $0.backgroundColor = .colorF8F6FF
         $0.layer.cornerRadius = 12
     }
-    private let workoutTimeLabel = UILabel().then {
+    private let workoutTimeBaseView = UIView()
+    
+    private let workoutTimeTitleLabel = UILabel().then {
         $0.textColor = .color7442FF
         $0.text = "운동 시간"
         $0.font = .pretendard(.semiBold, size: 12)
     }
-    private let workoutTypeLabel = UILabel().then {
+    private let workoutTimeLabel = UILabel().then {
+        $0.text = "00:00:00"
+        $0.font = .pretendard(.light, size: 22)
+        $0.textColor = .color000000
+    }
+    
+    private let workoutTypeBaseView = UIView()
+    
+    private let workoutTypeTitleLabel = UILabel().then {
         $0.textColor = .color7442FF
         $0.text = "운동 종목"
         $0.font = .pretendard(.semiBold, size: 12)
+    }
+    private let workoutTypeLabel = UILabel().then {
+        $0.text = "-"
+        $0.textColor = .color000000
+        $0.font = .pretendard(.light, size: 22)
     }
     let workoutResultButton = UIButton()
     
     override func setupLayout() {
         super.setupLayout()
-        self.addSubviews(workoutResultLabel, workoutResultBaseView)
-        workoutResultBaseView.addSubview(workoutResultButton)
+        self.addSubviews(workoutResultLabel, workoutResultBaseView, workoutResultButton)
+//        workoutResultBaseView.addSubview(workoutResultButton)
+        workoutResultBaseView.addSubviews(workoutTimeBaseView, workoutTypeBaseView)
+        workoutTimeBaseView.addSubviews(workoutTimeTitleLabel, workoutTimeLabel)
+        workoutTypeBaseView.addSubviews(workoutTypeTitleLabel, workoutTypeLabel)
     }
     override func setupConstraints() {
         super.setupConstraints()
@@ -49,6 +67,34 @@ class WorkoutResultView : BaseUIView {
             $0.centerX.equalToSuperview()
             $0.leading.equalToSuperview().offset(18)
             $0.height.equalTo(82)
+        }
+        workoutTimeBaseView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(2)
+        }
+        workoutTypeBaseView.snp.makeConstraints {
+            $0.top.trailing.bottom.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(2)
+        }
+        workoutTimeTitleLabel.snp.makeConstraints {
+            $0.height.equalTo(25)
+            $0.top.equalToSuperview().inset(11)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        workoutTimeLabel.snp.makeConstraints {
+            $0.height.equalTo(26)
+            $0.top.equalTo(workoutTimeTitleLabel.snp.bottom)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        workoutTypeTitleLabel.snp.makeConstraints {
+            $0.height.equalTo(25)
+            $0.top.equalToSuperview().inset(11)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        workoutTypeLabel.snp.makeConstraints {
+            $0.height.equalTo(26)
+            $0.top.equalTo(workoutTypeTitleLabel.snp.bottom)
+            $0.leading.equalToSuperview().inset(20)
         }
         workoutResultButton.snp.makeConstraints {
             $0.edges.equalToSuperview()
